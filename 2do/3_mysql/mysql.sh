620aedf9b9d19d4fd481cb173c4ba0a2620fa778
mikevgb <mikevgb@users.noreply.github.com>
2020-12-26 22:29:14 +0100

    auto_up

--- 2do/3_mysql/mysql.sh
+++ 2do/3_mysql/mysql.sh
@@ -4,5 +4,5 @@ kubectl run mysql --image=alpine -- sleep infinity
 #install mysql in pod
 kubectl exec mysql -- apk add mysql mysql-client
 
-#create database
+#create database (!!!! not working)
 kubectl exec mysql -- mysql -u root --skip-password
